775ca460895f59772ee8fe95d116311b3ec370e0
Fernando <fernandoeqp59@gmail.com>
2024-11-19 14:55:58 -0300

    Projeto Finalizado

---
+++ Script/planos.js
@@ -0,0 +1,273 @@
+const monthMain = ` 
+<div class="col col1">
+    <h1>Starter</h1>
+    <small>Ideal for freelance and contractors just starting out</small>
+    <div class="preco">
+        <span class="cifrao">$</span>
+        <span class="num">24</span>
+        <div class="month-text">
+            <span class="month-num">/MONTH</span>
+        </div>
+    </div>
+
+    <div class="list">
+        <ul class="checklist">
+            <li>All Templates</li>
+            <li>Unimited Client & Project</li>
+            <li>Invoicing & Payments</li>
+            <li>Proposais & Contacts</li>
+            <li>Task & Time Tracking</li>
+            <li>Client CRM</li>
+            <li>Expense Tracking</li>
+            <li>Up to 5 Project Collaborators</li>
+        </ul>
+    </div>
+    <div class="btn-free">
+        <button>START FREE</button>
+    </div>
+</div>
+
+
+<div class="col col2">
+    <div class="popular"><span>MOST POPULAR</span></div>
+
+    <h1>Professional</h1>
+    <small>Everything a growing independent business needs to thrive.</small>
+    <div class="preco">
+        <span class="cifrao">$</span>
+        <span class="num">39</span>
+        <div class="month-text">
+            <span class="month-num">/MONTH</span>
+        </div>
+    </div>
+
+    <div class="list">
+        <ul class="checklist">
+            <li>Everything in Starter plus...</li>
+            <li>Custom Branding</li>
+            <li>Forms & Questionnaires</li>
+            <li>Proposais & Contacts</li>
+            <li>Workflow Automations</li>
+            <li>Client Portal</li>
+            <li>Calendly Integration</li>
+            <li>Zapier Integration</li>
+            <li>Up to 15 Project Collaborators</li>
+        </ul>
+    </div>
+    <div class="btn-free">
+        <button>START FREE</button>
+    </div>
+</div>
+
+
+<div class="col col3">
+    <h1>Business</h1>
+    <small>The perfect package for small businesses and agencies.</small>
+    <div class="preco">
+        <span class="cifrao">$</span>
+        <span class="num">79</span>
+        <div class="month-text">
+            <span class="month-num">/MONTH</span>
+        </div>
+    </div>
+
+
+    <div class="list">
+        <ul class="checklist">
+            <li>Everything in Starter and Professional plus...</li>
+            <li>Subcontractor Management</li>
+            <li>Hiring Agreement Templates (1099 contracts)</li>
+            <li>Subcontractor Onboarding</li>
+            <li>Talent Pool</li>
+            <li>3 Team Seats (additional seats $9/month)</li>
+            <li>Accountant Access</li>
+            <li>Connect Multiple Bank Accounts</li>
+            <li>Unlimited Subcontractors</li>
+            <li>Unlimited Project Collaborators</li>
+    </div>
+    <div class="btn-free">
+        <button>START FREE</button>
+    </div>
+</div>
+`;
+
+const monthSub = ` <div class="row-Plano">
+                <div class="info-plano">
+                    <h4>Collaborators</h4>
+                    <p>Invite other users to specific projects for limited access and functionality.</p>
+                </div>
+
+                <div class="valor-plano">
+                    <span>$Free</span>
+                </div>
+            </div>
+
+            <!--Plano 2-->
+            <div class="row-Plano">
+                <div class="info-plano">
+                    <h4>Partners</h4>
+                    <p>Invite other users for full account access and company management.</p>
+                </div>
+
+                <div class="valor-plano">
+                    <span>$9</span>
+                    <small>/MONTH</small>
+                </div>
+            </div>
+
+            <!--Plano 3-->
+            <div class="row-Plano">
+                <div class="info-plano">
+                    <h4>Bonsai Tax</h4>
+                    <p>Track expenses, identify write-offs, and estimate quarterly taxes easily.</p>
+                      <a href="#">LEARN MORE <span> ></span></a>
+                </div>
+
+                <div class="valor-plano">
+                    <p>$10</p>
+                     <small>/MONTH</small>
+                </div>
+            </div>`;
+
+const yearMain = `
+   
+            <div class="col col1">
+                <h1>Starter</h1>
+                <small>Ideal for freelance and contractors just starting out</small>
+                <div class="preco">
+                    <span class="cifrao">$</span>
+                    <span class="num">17</span>
+                    <div class="month-text">
+                        <span class="month-num">/MONTH</span>
+                    </div>
+                </div>
+                <span class="bill">Billed yearly</span>
+
+                <div class="list">
+                    <ul class="checklist">
+                        <li>All Templates</li>
+                        <li>Unimited Client & Project</li>
+                        <li>Invoicing & Payments</li>
+                        <li>Proposais & Contacts</li>
+                        <li>Task & Time Tracking</li>
+                        <li>Client CRM</li>
+                        <li>Expense Tracking</li>
+                        <li>Up to 5 Project Collaborators</li>
+                    </ul>
+                </div>
+                <div class="btn-free">
+                    <button>START FREE</button>
+                </div>
+            </div>
+
+            <div class="col col2">
+                <div class="popular"><span>MOST POPULAR</span></div>
+
+                <h1>Professional</h1>
+                <small>Everything a growing independent business needs to thrive.</small>
+                <div class="preco">
+                    <span class="cifrao">$</span>
+                    <span class="num">32</span>
+                    <div class="month-text">
+                        <span class="month-num">/MONTH</span>
+                    </div>
+                </div>
+                <span class="bill">Billed yearly</span>
+
+                <div class="list">
+                    <ul class="checklist">
+                        <li>Everything in Starter plus...</li>
+                        <li>Custom Branding</li>
+                        <li>Forms & Questionnaires</li>
+                        <li>Proposais & Contacts</li>
+                        <li>Workflow Automations</li>
+                        <li>Client Portal</li>
+                        <li>Calendly Integration</li>
+                        <li>Zapier Integration</li>
+                        <li>Up to 15 Project Collaborators</li>
+                    </ul>
+                </div>
+                <div class="btn-free">
+                    <button>START FREE</button>
+                </div>
+            </div>
+
+            
+            <div class="col col3">
+                <h1>Business</h1>
+                <small>The perfect package for small businesses and agencies.</small>
+                <div class="preco">
+                    <span class="cifrao">$</span>
+                    <span class="num">52</span>
+                    <div class="month-text">
+                        <span class="month-num">/MONTH</span>
+                    </div>
+                </div>
+                <span class="bill">Billed yearly</span>
+
+                <div class="list">
+                    <ul class="checklist">
+                        <li>Everything in Starter and Professional plus...</li>
+                        <li>Subcontractor Management</li>
+                        <li>Hiring Agreement Templates (1099 contracts)</li>
+                        <li>Subcontractor Onboarding</li>
+                        <li>Talent Pool</li>
+                        <li>3 Team Seats (additional seats $9/month)</li>
+                        <li>Accountant Access</li>
+                        <li>Connect Multiple Bank Accounts</li>
+                        <li>Unlimited Subcontractors</li>
+                        <li>Unlimited Project Collaborators</li>
+                </div>
+                <div class="btn-free">
+                    <button>START FREE</button>
+                </div>
+            </div>
+    `;
+const yearSub = ` <div class="row-Plano">
+                <div class="info-plano">
+                    <h4>Accounting & Tax Assistant</h4>
+                    <p>Manage your freelance finances and always be ready for tax season with easy-to-use accounting and
+                        tax tools.</p>
+                    <a href="#">LEARN MORE <span> ></span></a>
+                </div>
+
+                <div class="valor-plano">
+                    <span>$100</span>
+                    <small>/YEAR</small>
+                </div>
+            </div>
+
+            <!--Plano 2-->
+            <div class="row-Plano">
+                <div class="info-plano">
+                    <h4>Partners</h4>
+                    <p>Invite other users for full account access and company management.</p>
+                </div>
+
+                <div class="valor-plano">
+                    <span>$90</span>
+                    <small>/YEAR</small>
+                </div>
+            </div>
+
+            <!--Plano 3-->
+            <div class="row-Plano">
+                <div class="info-plano">
+                    <h4>Collaborators</h4>
+                    <p>Invite other users to specific projects for limited access and functionality.</p>
+                </div>
+
+                <div class="valor-plano">
+                    <span>$Free</span>
+                </div>
+            </div>`;
+
+function main(id) {
+  if (id === `flexRadioDefault1`) {
+    document.getElementById(`mainPrincipal`).innerHTML = monthMain;
+    document.getElementById(`planosSub`).innerHTML = monthSub;
+  } else {
+    document.getElementById(`mainPrincipal`).innerHTML = yearMain;
+    document.getElementById(`planosSub`).innerHTML = yearSub;
+  }
+}
